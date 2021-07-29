ce27b97414a0b9497063dd5fbe938c4d27324c66
vivekthapliyal4 <vivekthapliyal4@gmail.com>
2021-07-29 21:17:39 +0530

    initial commit

---
+++ js/app.js
@@ -0,0 +1,198 @@
+const genreCover = [
+    {
+        id: 1,
+        name: "Podcasts",
+        background: "rgb(39, 133, 106)",
+        img: "https://i.scdn.co/image/567158eb895ad26718a814345af0fc43ee785ec5",
+    },
+    {
+        id: 2,
+        name: "Made For You",
+        background: "#1e3264",
+        img: "https://t.scdn.co/images/ea364e99656e46a096ea1df50f581efe",
+    },
+    {
+        id: 3,
+        name: "Charts",
+        background: " rgb(141, 103, 171)",
+        img: "https://charts-images.scdn.co/assets/locale_en/regional/weekly/region_global_default.jpg",
+    },
+    {
+        id: 4,
+        name: "New Releases",
+        background: "#e8115b",
+        img: "https://i.scdn.co/image/ab67706f000000027ea4d505212b9de1f72c5112",
+    },
+    {
+        id: 5,
+        name: "Discover",
+        background: "rgb(141, 103, 171)",
+        img: "https://t.scdn.co/images/d0fb2ab104dc4846bdc56d72b0b0d785.jpeg",
+    },
+    {
+        id: 6,
+        name: "Concerts",
+        background: "rgb(30, 50, 100)",
+        img: "https://t.scdn.co/images/8cfa9cb1e43a404db76eed6ad594057c",
+    },
+    {
+        id: 7,
+        name: "At Home",
+        background: "rgb(71, 125, 149)",
+        img: "https://i.scdn.co/image/ab67706f00000002ec9d60059aa215a7ba364695",
+    },
+    {
+        id: 8,
+        name: "COVID-19 Guide",
+        background: "rgb(80, 155, 245)",
+        img: "https://i.scdn.co/image/0e121a9a02c17d6703e7c12ba0c704166f9afac9",
+    },
+    {
+        id: 9,
+        name: "Indie",
+        background: "rgb(255, 70, 50)",
+        img: "https://t.scdn.co/images/29d2df7525b64ed49889c93d06c9e83b.jpeg",
+    },
+    {
+        id: 10,
+        name: "EQUAL",
+        background: "rgb(20, 138, 8)",
+        img: "https://i.scdn.co/image/ab67706f0000000284a1ec26f589f0d569805a07",
+    },
+    {
+        id: 11,
+        name: "Pop",
+        background: "rgb(175, 40, 150)",
+        img: "https://t.scdn.co/images/0a74d96e091a495bb09c0d83210910c3",
+    },
+    {
+        id: 12,
+        name: "Radar",
+        background: "rgb(25, 230, 140)",
+        img: "https://t.scdn.co/images/c6677aa51acf4121b66b9d1f231bd427.png",
+    },
+    {
+        id: 13,
+        name: "Tokyo",
+        background: "rgb(175, 40, 150)",
+        img: "https://t.scdn.co/images/0e5a2f7ea4bf44ca88be919b84a39912",
+    },
+    {
+        id: 14,
+        name: "Indian Classical",
+        background: "rgb(96, 129, 8)",
+        img: "https://t.scdn.co/images/0dcbe54ffb604b37b1ae96223f2524be",
+    },
+    {
+        id: 15,
+        name: "Party",
+        background: "rgb(141, 103, 171)",
+        img: "	https://i.scdn.co/image/ab67706f00000002caa115cbdb8cd3d39d67cdc0",
+    },
+    {
+        id: 16,
+        name: "K-Pop",
+        background: "rgb(180, 155, 200)",
+        img: "https://i.scdn.co/image/ab67706f00000002978b9f4a4f40b430fd0d837e",
+    },
+    {
+        id: 17,
+        name: "Romance",
+        background: "rgb(240, 55, 165)",
+        img: "https://i.scdn.co/image/ab67706f0000000213601d4833623a4d6b328e38",
+    },
+    {
+        id: 18,
+        name: "Workout",
+        background: "rgb(156, 240, 225)",
+        img: "https://i.scdn.co/image/ab67706f000000029249b35f23fb596b6f006a15",
+    },
+    {
+        id: 19,
+        name: "Hip Hop",
+        background: "rgb(141, 103, 171)",
+        img: "https://i.scdn.co/image/ab67706f000000029bb6af539d072de34548d15c",
+    },
+    {
+        id: 20,
+        name: "Folks & Acoustic",
+        background: "rgb(30, 50, 100)",
+        img: "https://i.scdn.co/image/ab67706f0000000237df164786f688dd0ccd8744",
+    },
+    {
+        id: 21,
+        name: "Sleep",
+        background: "rgb(140, 25, 50)",
+        img: "https://i.scdn.co/image/ab67706f00000002b70e0223f544b1faa2e95ed0",
+    },
+    {
+        id: 22,
+        name: "Rock",
+        background: "rgb(20, 138, 8)",
+        img: "https://i.scdn.co/image/ab67706f00000002fe6d8d1019d5b302213e3730",
+    },
+    {
+        id: 23,
+        name: "Wellness",
+        background: "rgb(175, 40, 150)",
+        img: "https://i.scdn.co/image/ab67706f00000002ec9d60059aa215a7ba364695",
+    },
+    {
+        id: 24,
+        name: "Jazz",
+        background: "rgb(119, 119, 119)",
+        img: "https://i.scdn.co/image/ab67706f00000002d72ef75e14ca6f60ea2364c2",
+    },
+    {
+        id: 25,
+        name: "Blues",
+        background: "rgb(141, 103, 171)",
+        img: "https://i.scdn.co/image/ab67706f0000000285c3e89f02770deb7691451c",
+    },
+    {
+        id: 26,
+        name: "Metal",
+        background: "rgb(71, 125, 149)",
+        img: "https://i.scdn.co/image/ab67706f0000000285704160b49125ac95099ec8",
+    },
+    {
+        id: 27,
+        name: "Classical",
+        background: "rgb(186, 93, 7)",
+        img: "https://i.scdn.co/image/ab67706f000000023e0130fcd5d106f1402b4707",
+    },
+    {
+        id: 28,
+        name: "Student",
+        background: "rgb(235, 30, 50)",
+        img: "	https://t.scdn.co/images/67a216e1e07144f889d366fed5c325bc.jpeg",
+    },
+
+];
+
+
+
+window.addEventListener('DOMContentLoaded', function(){
+    genreCover.forEach(function addItem(genre) {
+        const genres = document.querySelector('.genres');
+        const genreCard = document.createElement('div');
+        genreCard.className = 'genre-card';
+        genreCard.style.background = `${genre.background}`
+
+        genreCard.innerHTML = 
+            `<h3>${genre.name}</h3>
+            <img src="${genre.img}" alt="${genre.name}">`
+        
+
+        genres.appendChild(genreCard);
+
+        
+
+    });
+})
+
+
+
+
+
+AOS.init();
